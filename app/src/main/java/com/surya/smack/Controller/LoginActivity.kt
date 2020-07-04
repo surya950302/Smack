53808871181d41c3b4a8b956a4d98c3ba36a1864
surya <surya950302@gmail.com>
2020-07-04 00:33:14 -0400

    finished logIn feature

--- app/src/main/java/com/surya/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/surya/smack/Controller/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.surya.smack.R
+import com.surya.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginOnClicked(view : View){
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
 
+        AuthService.loginUser(this, email,password){loginSuccess ->
+            if(loginSuccess){
+                AuthService.finfUserByEmail(this){findSuccess ->
+                    if(findSuccess){
+                        finish()
+                    }
+
+                }
+            }
+
+        }
     }
     fun loginSignUpOnClicked(view : View){
         val signUpIntent = Intent(this, CreateUserActivity:: class.java)
--- app/src/main/java/com/surya/smack/Services/AuthService.kt
+++ app/src/main/java/com/surya/smack/Services/AuthService.kt
@@ -1,14 +1,14 @@
 package com.surya.smack.Services
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.surya.smack.Utilities.URL_CREATE_USER
-import com.surya.smack.Utilities.URL_LOGIN
-import com.surya.smack.Utilities.URL_REGISTER
+import com.surya.smack.Utilities.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.lang.Exception
@@ -64,7 +64,7 @@ object AuthService {
 
 
         },Response.ErrorListener {error ->
-            Log.d("ERROR","EXC : ${error.printStackTrace()}")
+            Log.d("ERROR","Could not login user : ${error.printStackTrace()}")
             complete(false)
         }){
             override fun getBodyContentType(): String {
@@ -115,6 +115,7 @@ object AuthService {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
                 try{
+                    Log.d("Auth Token","$authToken")
                     headers.put("Authorization", "Bearer $authToken")
                 }catch (e: Exception){
                     println("The token is not defined: ${e.printStackTrace()}")
@@ -126,6 +127,46 @@ object AuthService {
         Volley.newRequestQueue(context).add(creatUserRequest)
     }
 
+    fun finfUserByEmail(context: Context, complete: (Boolean) -> Unit){
 
+        val findUserRequest = object : JsonObjectRequest(Method.GET, "$URL_GET_USER$userEmail", null, Response.Listener { response ->
+            try{
+                UserDataService.name = response.getString("name")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.avatarColor = response.getString("avatarColor")
+                UserDataService.id = response.getString("_id")
+
+                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                LocalBroadcastManager.getInstance(context).sendBroadcast(userDataChange)
+
+                complete(true)
+
+            }catch(e : JSONException){
+                Log.d("JSON Exception","EXC : ${e.printStackTrace()}")
+                complete(false)
+            }
+
+        },Response.ErrorListener {error ->
+            Log.d("ERROR","could not get user : ${error.printStackTrace()}")
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                try{
+                    //Log.d("Auth Token","$authToken")
+                    headers.put("Authorization", "Bearer $authToken")
+                }catch (e: Exception){
+                    println("The token is not defined: ${e.printStackTrace()}")
+                }
+                return headers
+            }
+        }
+        Volley.newRequestQueue(context).add(findUserRequest)
+    }
 
 }
